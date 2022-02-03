6e7e16191d7bd937b2bde432b0c744db46109901
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-02-02 16:26:41 -0800

    Add information about main projects

--- app/components/MainProjects.tsx
+++ app/components/MainProjects.tsx
@@ -5,7 +5,9 @@ import Memory from "../assets/projects/main/memory.png";
 import Shop from "../assets/projects/main/shop.png";
 import Spotter from "../assets/projects/main/spotter.png";
 import Clipboard from "../assets/projects/main/clipboard.png";
+
 import { FaGithub } from "react-icons/fa";
+import { ImNpm } from "react-icons/im";
 
 import { ExternalLinkIcon } from "@heroicons/react/outline";
 interface Props {}
@@ -16,53 +18,53 @@ const MainProjects: React.FC<Props> = ({}) => {
       name: "useTypewriter Hook",
       description:
         "A flexible hook for creating typewriter-like experience with React. Users can add functions for further applications.",
-      technologies: "React.js, Next.js, Javascript",
+      technologies: "React.js, Next.js, TypeScript, CSS",
       gitRepo: "https://github.com/alissanguyen/react-use-typewriter",
-      website: "https://react-use-typewriter.vercel.app/",
+      website: "https://usetypewriter.com/",
       npm: "https://www.npmjs.com/package/use-typewriter-hook",
       icon: Typewriter,
     },
     {
       name: "Memory Game",
-      description:
-        "My own personal blog built with Remix, Prisma, and TailwindCSS",
-      technologies:
-        "Remix, Javascript, TailwindCSS, Javascript, Prisma, SQLite",
-      gitRepo: "https://github.com/alissanguyen/portfolio-website",
-      website: "https://alissanguyen.dev/",
+      description: "A memory game website.",
+      technologies: "JavaScript, TypeScript, HTML & CSS",
+      gitRepo: "https://github.com/alissanguyen/memory-game",
+      website: "https://memory.alissanguyen.dev/",
       icon: Memory,
     },
     {
       name: "Dont Buy From Me",
       description:
         "A demo shopping website with add to cart features and purchases enabled",
-      technologies: "React.js, Remix, Javascript, Stripe, TailwindCSS",
+      technologies: "Remix, Javascript, Stripe, TailwindCSS, Prisma",
       gitRepo: "https://github.com/alissanguyen/dont-buy-from-me",
       website: "http://www.dontbuyfrom.me/",
       icon: Shop,
     },
     {
       name: "Clipboard Demo Page",
-      description: "A package for hamburger menu. Easy to use and implement.",
-      technologies: "Remix, React, Javascript",
-      gitRepo: "",
-      website: "",
+      description:
+        "A responsive landing page for a tool called Clipboard with animations.",
+      technologies: "React,js, Javascript, HTML & SCSS",
+      gitRepo: "https://github.com/alissanguyen/clipboard-page",
+      website: "https://clipboard.alissanguyen.dev",
       icon: Clipboard,
     },
     {
       name: "Github Spotter 2.0",
-      description: "A clone website of Amazon",
-      technologies: "Remix, React, Javascript",
-      gitRepo: "",
-      website: "",
+      description:
+        "A website designed to search GitHub users by usernames with Github API",
+      technologies: "React.js, Javascript, HTML, SCSS & CSS",
+      gitRepo: "https://github.com/alissanguyen/github-spotter-2",
+      website: "https://githubspotter2.alissanguyen.dev",
       icon: Spotter,
     },
     {
       name: "Crowdfund",
-      description: "A clone version of Netflix.",
-      technologies: "Remix, React, Javascript",
-      gitRepo: "",
-      website: "",
+      description: "A responsive demo landing page for crowdfunding projects.",
+      technologies: "React.js, Javascript, HTML & CSS",
+      gitRepo: "https://github.com/alissanguyen/demo-crowdfunding-page",
+      website: "https://crowdfund.alissanguyen.dev",
       icon: Crowdfund,
     },
   ];
@@ -78,8 +80,11 @@ const MainProjects: React.FC<Props> = ({}) => {
           <div className="main-project-header flex flex-row items-center justify-between">
             <img src={project.icon} className="w-7" alt="" />
             <div className="inline-flex items-center">
+              {project.npm ? (
+                <ImNpm className="w-6 mr-2 hover:text-red-600" />
+              ) : null}
               <FaGithub className="w-6 mr-2 hover:text-sky-600" />
-              <ExternalLinkIcon className="w-7 hover:text-sky-600" />
+              <ExternalLinkIcon className="w-7 hover:text-teal-500" />
             </div>
           </div>
           <div className="main-project-info flex flex-col justify-between mt-5">
