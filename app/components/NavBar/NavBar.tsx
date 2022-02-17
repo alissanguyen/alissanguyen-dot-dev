a00218e65d817734b8f68177894ab13cd09c9023
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-02-16 22:37:57 -0800

    Add responsiveness for screens < 460px for navbar and intro section

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -25,26 +25,28 @@ const NavBar: React.FC = (props) => {
   return (
     <div className="nav-bar-wrapper w-full">
       <StripeNavbar />
-      <nav className="navbar mx-auto max-w-screen-lg px-14 lg:px-0 py-10 flex flex-row items-center justify-between text-navBar-link text-lg">
-        <div className="nav-logo text-2xl font-medium uppercase text-navBar-linkHover hover:text-navBar-link underlined">
+      <nav className="desktop-navbar mx-auto max-w-screen-lg w-full px-14 lg:px-0 py-10 flex flex-row items-center justify-between text-navBar-link text-xl grid-flow-col">
+        <div className="nav-logo text-3xl font-medium uppercase text-navBar-linkHover hover:text-navBar-link underlined">
           <a href="/">Alissa N</a>
         </div>
         {topLevelLinks.map((link) => (
           <a
-            className="nav-link dark underlined hover:text-navBar-linkHover links-wrapper"
+            className="nav-link dark underlined hover:text-navBar-linkHover links-wrapper w-fit"
             href={link.href}
           >
             {link.displayName}
           </a>
         ))}
-
-        <div className="mobile-menu">
-          <ResponsiveNavMenu links={topLevelLinks} />
-        </div>
-        <div className="desktop-theme-btn">
+        <div className="desktop-theme-btn col-span-1">
           <ThemeButton />
         </div>
       </nav>
+      <div className="mobile-navbar-wrapper flex max-w-screen-lg w-full px-14 py-10">
+        <div className="nav-logo text-3xl font-medium uppercase text-navBar-linkHover hover:text-navBar-link underlined">
+          <a href="/" className="flex w-max">Alissa N</a>
+        </div>
+        <ResponsiveNavMenu links={topLevelLinks} />
+      </div>
     </div>
   );
 };
--- app/components/NavBar/ResponsiveNavMenu.tsx
+++ app/components/NavBar/ResponsiveNavMenu.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { redirect } from "remix";
 import { useModalContext } from "~/providers/ModalProvider";
 import ThemeButton from "../ThemeButton/ThemeButton";
 
@@ -14,14 +15,27 @@ const ResponsiveNavMenu: React.FC<Props> = (props) => {
   };
 
   return (
-    <div className={`mobile-nav w-full h-full m-0 p-0 ${modalIsOpen ? "open" : ""}`}>
-      <button className="mobile-nav-menu-toggle" onClick={handleClick}></button>
-      <nav>
-        <ul className="mobile-nav-menu">
-          <li data-text="Home">Home</li>
-          <li data-text="Projects">Projects</li>
-          <li data-text="About">About</li>
-          <li data-text="Contact">Contact</li>
+    <div
+      className={`mobile-nav w-full h-full m-0 p-0 ${
+        modalIsOpen ? "open" : ""
+      }`}
+    >
+      <button
+        className="mobile-nav-menu-opener bg-transparent fixed b-0 before:absolute before:top-0 before:right-0 before:m-auto before:bottom-0 before:w-full opacity-70 hover:opacity-100 after:opacity-0 after:absolute"
+        onClick={handleClick}
+      ></button>
+      <nav className="fixed z-10 left-0 w-full h-full overflow-hidden before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:m-auto before:w-full before:h-0 before:overflow-hidden">
+        <ul className="mobile-nav-menu m-0 list-none p-0 h-max fixed">
+          {props.links.map((link) => (
+            <a href={link.href} onClick={handleClick}>
+              <li className="mobile-nav-link opacity-0 h-full w-full flex items-center justify-center before:absolute before:top-0 before:right-0 before:bottom-0 before:left-auto before:m-auto before:h-full before:w-0 before:overflow-hidden after:absolute after:flex after:items-center after:justify-center after:top-0 after:right-0 after:bottom-0 after:left-0 after:m-auto after:overflow-hidden text-4xl">
+                {link.displayName}
+              </li>
+            </a>
+          ))}
+          <li className="w-full h-full flex items-center justify-center">
+            <ThemeButton />
+          </li>
         </ul>
       </nav>
     </div>
