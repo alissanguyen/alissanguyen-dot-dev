80b887c6ce18394dbfc0c097b2ffdfb2e224ec6f
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-03-02 19:57:54 -0800

    Remove ability for users to highlight text on the Download Resume button.

--- app/components/ResumeButton/ResumeButton.tsx
+++ app/components/ResumeButton/ResumeButton.tsx
@@ -1,17 +1,8 @@
 import * as React from "react";
-import { LinksFunction } from "remix";
-import styles from "./ResumeButton.css";
-
-export const links: LinksFunction = () => [
-  {
-    rel: "stylesheet",
-    href: styles
-  }
-];
 
 const ResumeButton: React.FC = () => {
   return (
-    <div className="resume-button px-5 py-2 text-center uppercase relative after:absolute after:bottom-0 after:w-0 after:left-1/2 after:bg-transparent hover:cursor-pointer focus:cursor-pointer lg:text-base font-normal">
+    <div className="resume-button px-5 py-2 text-center uppercase relative after:absolute after:bottom-0 after:w-0 after:left-1/2 after:bg-transparent hover:cursor-pointer focus:cursor-pointer lg:text-base font-normal select-none">
       Download Resume
     </div>
   );
