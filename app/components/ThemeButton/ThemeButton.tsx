0afe9bb01b66afe13151d2b4ea9d19d2dc8d8e82
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-03-03 11:34:54 -0800

    Change theme toggle button styles (more simple and modern design

--- app/components/ThemeButton/ThemeButton.tsx
+++ app/components/ThemeButton/ThemeButton.tsx
@@ -1,10 +1,31 @@
-import { MoonIcon, SunIcon } from "@heroicons/react/solid";
 import * as React from "react";
 import { useTheme } from "~/providers/ThemeProvider";
 import { SupportedTheme } from "~/types";
+import Moon from "../../assets/moon.svg";
+interface Props {
+  hasStripeHeader: boolean;
+}
 
-const ThemeButton: React.FC = (props) => {
+const getShadowClassName = (
+  theme: SupportedTheme,
+  hasStripeHeader: boolean
+) => {
+  if (theme === SupportedTheme.LIGHT) {
+    return hasStripeHeader ? "shadow-light-stripe" : "shadow-light";
+  } else {
+    return hasStripeHeader ? "shadow-dark-stripe" : "shadow-dark";
+  }
+};
+
+const ThemeButton: React.FC<Props> = (props) => {
   const { theme, updateTheme } = useTheme();
+
+  console.log("hasStripeHeader", props.hasStripeHeader);
+
+  // Default design of theme button does not account for color constrast between box shadow and stripes header
+  const sun =
+    "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
+  const shadow = getShadowClassName(theme, props.hasStripeHeader);
 
   const handleToggleTheme = (oldTheme: SupportedTheme) => {
     updateTheme(
@@ -13,26 +34,19 @@ const ThemeButton: React.FC = (props) => {
         : SupportedTheme.DARK
     );
   };
-
   const nextTheme =
     theme === SupportedTheme.DARK ? SupportedTheme.LIGHT : SupportedTheme.DARK;
-
   const generateFormData = () => {
     const form = new FormData();
     form.append("theme", nextTheme);
     return form;
   };
 
-  const isChecked = theme === SupportedTheme.DARK ? false : true;
-
   return (
-    <div className="switch relative focus:outline-2 ">
-      <input
-        type="checkbox"
-        name="theme"
-        defaultChecked={isChecked}
-        value={theme}
-        onChange={() => {
+    <div className="ThemeButton_Wrapper">
+      <button
+        className={`theme-container ${shadow}`}
+        onClick={() => {
           handleToggleTheme(theme);
 
           fetch("/setTheme?index", {
@@ -41,22 +55,22 @@ const ThemeButton: React.FC = (props) => {
             credentials: "same-origin"
           });
         }}
-        className=" top-0 right-0 bottom-0 left-0 opacity-0 absolute w-full h-full cursor-pointer"
-      />
-      <label htmlFor="toggle" className="block h-full relative w-4/5">
-        {theme === SupportedTheme.DARK ? (
-          <div className="flex absolute">
-            <MoonIcon className="theme-icon moon-dark m-auto" />
-          </div>
+      >
+        {theme === SupportedTheme.LIGHT ? (
+          <img className="theme-icon" src={sun} alt="" />
         ) : (
-          <div className="flex absolute">
-            <SunIcon className="theme-icon sun-light m-auto" />
-          </div>
+          <MoonIcon />
         )}
-      </label>
-      <span className="inline-block absolute right-0"></span>
+      </button>
     </div>
   );
 };
 
+const MoonIcon = () => (
+  <img
+    src={Moon}
+    alt=""
+    className="theme-icon flex items-center m-auto justify-center w-8"
+  />
+);
 export default ThemeButton;
