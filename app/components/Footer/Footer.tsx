08e5b534eea3fb7db51819f794b5f1df623c9682
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-03-03 22:12:38 -0800

    Add animation for blog route footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,14 +1,32 @@
 import * as React from "react";
+import { useLocation } from "remix";
+import { useTheme } from "~/providers/ThemeProvider";
+import { SupportedTheme } from "~/types";
 
 interface Props {}
 
 const Footer: React.FC<Props> = ({}) => {
+  const location = useLocation();
+  const onBlogRoute = location.pathname.startsWith("/blog");
+
   return (
-    <div className="bottom-0 py-5 w-full flex flex-col items-center justify-center text-xs sm:text-sm text-textSmColor">
-      <p>Built and designed by Alissa Nguyen a.k.a Tam Nguyen.</p>
-      <p>Copyright © 2021 All Rights Reserved.</p>
+    <div className="w-full relative">
+      {onBlogRoute ? <Waves /> : null}
+      <div className="bottom-0 py-5 w-full flex flex-col items-center justify-center text-xs sm:text-sm text-textSmColor">
+        <p>Built and designed by Alissa Nguyen a.k.a Tam Nguyen.</p>
+        <p>Copyright © {new Date().getFullYear()} All Rights Reserved.</p>
+      </div>
     </div>
   );
 };
 
 export default Footer;
+
+const Waves = () => {
+  return (
+    <div className="ocean">
+      <div className="wave"></div>
+      <div className="wave"></div>
+    </div>
+  );
+};
