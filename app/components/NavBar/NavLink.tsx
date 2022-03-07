0e87111146553c822860ea550534c9eca61067b1
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-03-06 18:56:21 -0800

    Add nav link component

---
+++ app/components/NavBar/NavLink.tsx
@@ -0,0 +1,29 @@
+import clsx from "clsx";
+import { Link, useLocation } from "remix";
+
+const NavLink: React.FC<
+  Omit<Parameters<typeof Link>["0"], "to"> & { to: string }
+> = ({ to, ...rest }) => {
+  const location = useLocation();
+  const isSelected =
+    to === location.pathname || location.pathname.startsWith(`${to}/`);
+
+  return (
+    <li className="px-5 py-2">
+      <Link
+        prefetch="intent"
+        className={clsx(
+          "underlined focus:outline-none block whitespace-nowrap text-lg font-medium",
+          {
+            active: isSelected,
+            "text-secondary": !isSelected
+          }
+        )}
+        to={to}
+        {...rest}
+      />
+    </li>
+  );
+};
+
+export default NavLink;
