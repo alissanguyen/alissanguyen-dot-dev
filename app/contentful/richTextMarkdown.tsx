f35361feb43751c0b2b80e216fcf0bb125eb0e98
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-03-08 21:29:47 -0800

    Fix highlighted parser not working correctly when textNodes are not simple strings

--- app/contentful/richTextMarkdown.tsx
+++ app/contentful/richTextMarkdown.tsx
@@ -11,8 +11,12 @@ import { ContentfulQuote } from "./contentful";
 
 export const options: Options = {
   renderMark: {
-    [MARKS.BOLD]: (text) => <span className="bold font-bold text-post-bodyTextLg">{text}</span>,
-    [MARKS.ITALIC]: (text) => <span className="italic text-post-bodyTextLg">{text}</span>,
+    [MARKS.BOLD]: (text) => (
+      <span className="bold font-bold text-post-bodyTextLg">{text}</span>
+    ),
+    [MARKS.ITALIC]: (text) => (
+      <span className="italic text-post-bodyTextLg">{text}</span>
+    ),
     [MARKS.UNDERLINE]: (text) => <span className="underlined">{text}</span>,
     // TODO: ADD CUSTOM CODE STYLING
     [MARKS.CODE]: (text) => <code className="italic">{text}</code>
@@ -138,9 +142,29 @@ export const options: Options = {
  */
 const highlightedTextMatcher = /\((.+)\)(?=\[(\w+)\])/;
 
-const addColour = (children: React.ReactNode[] = []) => {
+/**
+ * The purpose of this function is to check if any of the passed in children
+ * or any of the passed in children's children meet the regex pattern of text
+ * inside parens followed by text inside brackets. If so, it returns a span
+ * wrapper around the string that meets the pattern. The span has a classname
+ * to apply some colored text and appropriate background color, aka a highlight.
+ * If it does not, just return the children as is.
+ */
+const addColour = (children: null | React.ReactNode[] = []) => {
+  if (children === null) {
+    /**
+     * Perhaps an overly defensive check, but we can't trust everything
+     * contentful gives us ;)
+     */
+    return children;
+  }
+
   // flatMap returns a flattened array - very useful
   const mappedChildren = children.flatMap((child) => {
+    /**
+     * child is a string when the node is normal text without any italicization,
+     * bolding, underlining, strikethrough, etc.
+     */
     if (typeof child === "string") {
       // the regex that handles parsing the actual string and extracting the text
       const matches = child.match(highlightedTextMatcher);
@@ -149,16 +173,49 @@ const addColour = (children: React.ReactNode[] = []) => {
         return result;
       }
     }
-    if (child !== null && typeof child === "object") {
+
+    /**
+     *
+     */
+    if (typeof child === "object") {
       const element = child as JSX.Element;
 
       const content = element.props.children;
+
       const className = element.props.className;
-      const matches =
+      const stringMatches =
         typeof content === "string" && content.match(highlightedTextMatcher);
 
-      if (matches) {
-        return createSpanFromMatches(matches, content, { className });
+      /**
+       * Handle the case where content is an object. This is when the highlighted text is also bolded or italicized, or both
+       */
+      if (
+        content &&
+        content.props &&
+        typeof content.props.children === "string"
+      ) {
+        const textContent = content.props.children;
+
+        const objectChildMatches = textContent.match(highlightedTextMatcher);
+
+        /**
+         * objectChildMatches will only be true when the child we're looking at
+         * meets the regex pattern of text inside parens followed by text inside
+         * brackets
+         */
+        if (objectChildMatches) {
+          return createSpanFromMatches(objectChildMatches, textContent, {
+            className
+          });
+        }
+      }
+
+      /**
+       * stringMatches will only be truthy when it meets the regex pattern of
+       * text inside parens followed by text inside brackets
+       */
+      if (stringMatches) {
+        return createSpanFromMatches(stringMatches, content, { className });
       }
     }
     // make sure to always return the content if there is no match to the regex
@@ -175,32 +232,21 @@ const createSpanFromMatches = (
 ) => {
   const content = text.split(`${matches[0]}[${matches[2]}]`);
 
-  //   const tempSentence = text.replace(`${matches[0]}[${matches[2]}]`, "## ##")
-  //   const sentence = tempSentence.split("##")
-
   // $TODO: this will cause more text than expected to be highlighted if there are multiple highlights within one html element
-
   return [
     content[0],
     <span
+      key={matches[1]}
       {...restProps}
-      style={{ color: "#000000", backgroundColor: `${contentfulHighlights[matches[2]]}` }}
+      style={{
+        color: "#000000",
+        backgroundColor: `${contentfulHighlights[matches[2]]}`
+      }}
     >
       {matches[1]}
     </span>,
     content[1]
   ];
-
-  //   return [...new Set(sentence)] // get the unique values / avoid ["", ""] - when there are no other parts of text
-  //     .map((text) =>
-  //       text === " " ? ( // map over the unique values to replace that which was split
-  //         <span {...restProps} style={{ backgroundColor: `${matches[2]}` }}>
-  //           {matches[1]}
-  //         </span> // return the element with the colour
-  //       ) : (
-  //         text
-  //       )
-  //     ); // or return the text
 };
 
 const contentfulHighlights: Record<string, string> = {
