a66a6e0b6253596f7027fbdef9d663bd2340a6e8
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-03-10 19:14:30 -0800

    Edit grid layout for skills section

--- app/sections/MySkills/MySkills.tsx
+++ app/sections/MySkills/MySkills.tsx
@@ -18,53 +18,50 @@ const MySkills: React.FC = ({}) => {
   return (
     <section className={`h-fit ${className}`} ref={setRef}>
       <div className="skills-section-container">
-        <div className="skills-section-and-chart-wrapper ">
-          <Title />
-          <img src="/images/background/Eclipse.svg" alt="" className="eclipse absolute" />
-          <div className="experience-wrapper grid xs:grid-rows-2 xs:gap-10 sm:grid-cols-7 gap-5 custom:gap-10">
-            <div
-              className={`skills-wrapper grid grid-cols-2 xs:grid-cols-3 custom2:grid-cols-4 sm:grid-cols-3 lg:grid-cols-4 gap-5 custom:gap-10 max-w-6xl sm:col-span-4 content-start`}
-            >
-              {skills.map((skill, index) => (
-                <div
-                  className={`skill-card ${className} relative left-0 top-0 w-full p-4 h-full w-full items-center text-center`}
-                  key={skill.id}
+        <img
+          src="/images/background/Eclipse.svg"
+          alt=""
+          className="eclipse absolute"
+        />
+        <Title />
+        <div className="skills-wrapper md:grid md:grid-cols-5 lg:grid-cols-7 gap-5">
+          <div className="grid grid-cols-2 custom3:grid-cols-3 custom2:grid-cols-4 md:grid-cols-2 md:col-span-2 lg:grid-cols-3 lg:col-span-3 gap-5 lg:gap-y-10">
+            {skills.map((skill, index) => (
+              <div
+                className={`skill-card ${className} relative left-0 top-0 w-full p-4 h-full w-full items-center text-center`}
+                key={skill.id}
+                style={{
+                  animationDelay: `${1 + index}`
+                }}
+              >
+                <div className="flex flex-col justify-between items-center">
+                  <img
+                    src={skill.icon}
+                    alt={skill.name}
+                    className="w-[3rem] h-[3rem]"
+                  />
+                  <p className="text-sm text-textSmColor mt-2">{skill.name}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <ul className="abilities-wrapper mt-5 md:mt-0 md:col-span-3 lg:col-span-4 flex flex-col justify-start">
+            {abilities.map((ability, index) => (
+              <div key={ability}>
+                <li
+                  className={`ability-card ${className} flex justify-center items-center text-left p-8`}
                   style={{
                     animationDelay: `${1 + index}`
                   }}
                 >
-                  <div className=" flex flex-col justify-between items-center">
-                    <img
-                      src={skill.icon}
-                      alt={skill.name}
-                      className="w-[3rem] h-[3rem]"
-                    />
-                    <p className="text-sm text-textSmColor mt-2">
-                      {skill.name}
-                    </p>
-                  </div>
-                </div>
-              ))}
-            </div>
-
-            <ul className="abilities-wrapper sm:col-span-3 ">
-              {abilities.map((ability, index) => (
-                <div key={ability}>
-                  <li
-                    className={`ability-card ${className} flex justify-center items-center text-left p-8`}
-                    style={{
-                      animationDelay: `${1 + index}`
-                    }}
-                  >
-                    <p className="text-base text-textSmColor leading-8">
-                      {ability}
-                    </p>
-                  </li>
-                  <div className="spacer-div mt-5"></div>
-                </div>
-              ))}
-            </ul>
-          </div>
+                  <p className="text-base text-textSmColor leading-8">
+                    {ability}
+                  </p>
+                </li>
+                <div className="spacer-div mt-5"></div>
+              </div>
+            ))}
+          </ul>
         </div>
       </div>
     </section>
@@ -108,16 +105,12 @@ const Title = () => {
       <span className={`${letterClassname} opacity-0`}>M</span>
       <span className={`${letterClassname} opacity-0`}>y</span>
       <span className={`${letterClassname} opacity-0`}>&nbsp;</span>
-      <span className={`${letterClassname} opacity-0`}>E</span>
-      <span className={`${letterClassname} opacity-0`}>x</span>
-      <span className={`${letterClassname} opacity-0`}>p</span>
-      <span className={`${letterClassname} opacity-0`}>e</span>
-      <span className={`${letterClassname} opacity-0`}>r</span>
+      <span className={`${letterClassname} opacity-0`}>S</span>
+      <span className={`${letterClassname} opacity-0`}>k</span>
       <span className={`${letterClassname} opacity-0`}>i</span>
-      <span className={`${letterClassname} opacity-0`}>e</span>
-      <span className={`${letterClassname} opacity-0`}>n</span>
-      <span className={`${letterClassname} opacity-0`}>c</span>
-      <span className={`${letterClassname} opacity-0`}>e</span>
+      <span className={`${letterClassname} opacity-0`}>l</span>
+      <span className={`${letterClassname} opacity-0`}>l</span>
+      <span className={`${letterClassname} opacity-0`}>s</span>
     </span>
   );
 };
