d8e3ba62ff412a8b6d0b42813a4e84444474da8d
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-03-10 19:13:41 -0800

    Add support for mobile users for copy url button

--- app/components/Blog/CopyURLButton.tsx
+++ app/components/Blog/CopyURLButton.tsx
@@ -3,21 +3,21 @@ import { FiCopy } from "react-icons/fi";
 import { BsCheck2 } from "react-icons/bs";
 
 interface Props {
-    handleCopyURL: (e: React.MouseEvent<HTMLButtonElement>) => void
-    userRecentlyCopiedText: boolean
+  handleCopyURL: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userRecentlyCopiedText: boolean;
 }
 
 const CopyURLButton: React.FC<Props> = (props) => {
   return (
     <>
       <button
-        className="big-url-button bg-white rounded-lg text-sm absolute px-4 hover:outline-2 outline-none hover:outline-amber-300 py-2 z-40"
+        className="big-url-button bg-white rounded-lg text-sm absolute px-4 hover:outline-2 outline-none hover:outline-amber-300 py-2 z-40 focus:outline-2 focus:outline-amber-300"
         onClick={props.handleCopyURL}
       >
         {props.userRecentlyCopiedText ? "Copied" : "Copy URL"}
       </button>
       <button
-        className="sm-url-button bg-white rounded-lg text-sm absolute focus:outline-2 focus:outline-amber-300 p-2 outline-none z-40"
+        className="sm-url-button bg-white rounded-lg text-sm absolute hover:outline-2 hover:outline-amber-300 focus:outline-2 focus:outline-amber-300 p-2 outline-none z-40"
         onClick={props.handleCopyURL}
       >
         {props.userRecentlyCopiedText ? (
