0f6cef4376b8fc445fcc04e71e4f3ee84940511d
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-03-20 19:54:43 -0700

    Improve styling of Titles section.

--- app/sections/AboutMe/Titles.tsx
+++ app/sections/AboutMe/Titles.tsx
@@ -4,15 +4,13 @@ const Titles: React.FC = ({}) => {
   return (
     <div className="title-content text-aboutMe-aboutMeText">
       <div className="title-content__container inline-flex overflow-hidden font-semibold items-center">
-        <p className="title-content__container__text m-0 float-left inline-flex">
-          I'm a
-        </p>
+        <p className="title-content__container__text m-0  inline-flex">I'm a</p>
         <span className="blinker">[</span>
-        <ul className="title-content__container__list text-left list-none">
+        <ul className="title-content__container__list text-center list-none">
           <li className="title-content__container__list__item m-0">
             software engineer
           </li>
-          <li  className="title-content__container__list__item m-0">
+          <li className="title-content__container__list__item m-0">
             freelance artist
           </li>
           <li className="title-content__container__list__item m-0">
