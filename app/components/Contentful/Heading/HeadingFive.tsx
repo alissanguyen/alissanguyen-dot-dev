c88d7b89adfaa639fe68a0497e97e4e1cd17bc3b
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-03-29 03:23:41 -0700

    Add responsiveness for paragraphs

--- app/components/Contentful/Heading/HeadingFive.tsx
+++ app/components/Contentful/Heading/HeadingFive.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 
 const HeadingFive: React.FC = (props) => {
   return (
-    <h5 className={`text-2xl text-post-bodyTextLg mt-2 mb-1 font-medium`}>
+    <h5
+      className={`BlogPost__HeadingFive text-xl xs:text-2xl text-post-bodyTextLg mt-2 mb-1 font-medium`}
+    >
       {props.children}
     </h5>
   );
--- app/components/Contentful/Heading/HeadingSix.tsx
+++ app/components/Contentful/Heading/HeadingSix.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 
 const HeadingSix: React.FC = (props) => {
-  return <h6 className={`text-xl mb-5 mt-10 font-medium`}>{props.children}</h6>;
+  return (
+    <h6 className={`font-bold text-xl mb-5 mt-10 xs:font-medium`}>
+      {props.children}
+    </h6>
+  );
 };
 
 export default HeadingSix;
