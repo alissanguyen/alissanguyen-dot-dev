0733a22617cdd30e8b9da91b0b55874df4de9f2e
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-03-29 19:21:04 -0700

    Edit ga.jsx to ga.ts

--- app/utils/ga.ts
+++ app/utils/ga.ts
@@ -1,12 +1,15 @@
-export function injectGA() {
+export function injectGA(): any {
   if (typeof window == "undefined") {
     return;
   }
+  // @ts-ignore
   window.dataLayer = window.dataLayer || [];
   function gtag() {
+    // @ts-ignore
     window.dataLayer.push(arguments);
   }
+  // @ts-ignore
   gtag("js", new Date());
-
+  // @ts-ignore
   gtag("config", "G-4J0L3BTY29");
 }
