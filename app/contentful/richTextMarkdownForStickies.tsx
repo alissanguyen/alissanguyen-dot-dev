2ae9343d20aba2619ba83951c7b2af2cb54f6fbc
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-09-01 16:59:35 -0700

    Add list item style

--- app/contentful/richTextMarkdownForStickies.tsx
+++ app/contentful/richTextMarkdownForStickies.tsx
@@ -87,7 +87,7 @@ export const stickyOptions: Options = {
     ),
     [BLOCKS.LIST_ITEM]: (node: any, children) => {
       return (
-        <li className="inline-flex list-item leading-8 ml-10">{children}</li>
+        <li className="inline-flex list-item text-lg leading-8 ml-10">{children}</li>
       );
     },
     [BLOCKS.HR]: (node: Node) => <div className="spacer-div h-7"></div>,
