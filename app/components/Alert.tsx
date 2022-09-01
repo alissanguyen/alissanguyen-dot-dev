c3b522e38c04facaceb807d5bd32d031e073cec0
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-09-01 16:51:35 -0700

    Modify contact form alerts

--- app/components/Alert.tsx
+++ app/components/Alert.tsx
@@ -24,13 +24,13 @@ const Alert: React.FC<Props> = (props) => {
                 {props.message}
               </div>
             </div>
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               className="flex-none fill-current text-green-600 h-3 w-3"
             >
               <path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z" />
-            </svg>
+            </svg> */}
           </div>
         </div>
       ) : props.type === AlertType.ERROR ? (
@@ -44,10 +44,7 @@ const Alert: React.FC<Props> = (props) => {
               <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z" />
             </svg>
             <div className="leading-tight flex flex-col space-y-2">
-              <div className="text-sm font-medium text-red-700">
-                Something went wrong
-              </div>
-              <div className="flex-1 leading-snug text-sm text-red-600">
+              <div className="flex-1 leading-tight text-sm text-red-600">
                 {props.message}
               </div>
             </div>
