c07a33465dfd70fce414721fd6e41b0cacbde96d
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-09-09 13:04:46 -0700

    Fix PWA issues

--- api/index.js
+++ api/index.js
@@ -3,3 +3,11 @@ const { createRequestHandler } = require("@remix-run/vercel");
 module.exports = createRequestHandler({
   build: require("./_build"),
 });
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    console.log('ServiceWorker registration successful with scope:',  registration.scope);
+  }).catch(function(error) {
+    console.log('ServiceWorker registration failed:', error);
+  });
+}
