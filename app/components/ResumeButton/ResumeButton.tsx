16725c26abd9896f4ae912fe1504d4cf38025975
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2023-02-04 16:04:05 -0800

    Restructure folders

--- app/components/ResumeButton/ResumeButton.tsx
+++ app/components/ResumeButton/ResumeButton.tsx
@@ -7,7 +7,7 @@ export const links: LinksFunction = () => {
 };
 const ResumeButton: React.FC = () => {
   return (
-    <a href="/images/AlissaNguyen_Resume.pdf" download={"AlissaNguyen_Resume.pdf"}>
+    <a href="/resume/AlissaNguyen_Resume.pdf" download={"AlissaNguyen_Resume.pdf"}>
       <button
         className="resume-button px-5 py-2 text-center uppercase relative after:absolute after:bottom-0 after:w-0 after:left-1/2 after:bg-transparent hover:cursor-pointer focus:cursor-pointer lg:text-base font-normal select-none rounded-xl"
         name="Download Resume"
