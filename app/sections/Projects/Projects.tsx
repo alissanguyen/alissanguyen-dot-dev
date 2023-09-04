8125418a890fcc918be33f1820791a43a0179427
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2023-09-04 16:09:23 -0700

    Remove featured title

--- app/sections/Projects/Projects.tsx
+++ app/sections/Projects/Projects.tsx
@@ -4,7 +4,6 @@ import RecentProjects from "./RecentProjects";
 import OtherProjects from "./OtherProjects";
 import styles from "./Projects.css";
 import { LinksFunction } from "@remix-run/node";
-import { useWasInViewAtLeastOnce } from "~/hooks/useWasInViewAtLeastOnce";
 
 export const links: LinksFunction = () => [
   {
@@ -14,22 +13,8 @@ export const links: LinksFunction = () => [
 ];
 
 const Projects: React.FC = ({}) => {
-  const { setRef, wasInViewAtLeastOnce } = useWasInViewAtLeastOnce();
-  const featuredClassname = wasInViewAtLeastOnce
-    ? "my-projects-title"
-    : undefined;
-
   return (
     <div className="projects-wrapper">
-      <svg
-        viewBox="0 0 1280 400"
-        className={`${featuredClassname} text-[16rem] lg:text-[15rem] font-medium`}
-        ref={setRef}
-      >
-        <text textAnchor="middle" x="50%" y="70%">
-          Featured
-        </text>
-      </svg>
       <FeaturedProjects />
       <div className="mt-24"></div>
       <RecentProjects />
