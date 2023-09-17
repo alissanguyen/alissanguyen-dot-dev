0b08a2f379f2c82e83632dad92a647900af93970
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2023-09-17 12:06:50 -0700

    Add contact form to AboutMe section

--- app/sections/AboutMe/AboutMe.tsx
+++ app/sections/AboutMe/AboutMe.tsx
@@ -6,50 +6,73 @@ import Hi from "./Hi";
 import { useTheme } from "~/providers/ThemeProvider";
 import ResumeButton from "~/components/ResumeButton/ResumeButton";
 import SocialMedia from "~/components/SocialMedia/SocialMedia";
+import contactStyles from "../../sections/Contact/Contact.css"
+import ContactMeSection from "~/sections/Contact/Contact";
+import { ContactFormFieldErrors } from "~/utils/functions";
+import { Transition } from "framer-motion";
 
 export const links: LinksFunction = () => [
   {
     rel: "stylesheet",
     href: styles
+  },
+  {
+    rel: "stylesheet",
+    href: contactStyles
   }
 ];
-const AboutMe: React.FC = (props) => {
+
+
+interface AboutMeProps {
+  actionData:
+  | { fieldErrors: Partial<ContactFormFieldErrors>; status: number }
+  | undefined,
+  transition: Transition
+}
+
+const AboutMe: React.FC<AboutMeProps> = (props) => {
   const { theme } = useTheme();
 
   return (
     <section id="AboutMe" className="AboutMe__Wrapper">
-      <article className="aboutme-wrapper flex flex-row">
-        <div className="Introduction__Wrapper" id="introductionWrapper">
-          <div className="flex flex-col sm:grid sm:grid-cols-5 sm:gap-5 mb-5">
-            <div className="flex flex-col col-span-3">
-              <Hi />
-              <div className="bio-description md:text-lg leading-10 text-textSmColor">
-                <div className="my-10 text-lg">
-                  <p className="my-4">
-                    Welcome to my personal website!
-                  </p>
-                  <p className="mb-5 xs:mb-10">
-                    I enjoy building software that makes peoples' lives easier
-                    by writing elegant, performant, and maintainable frontend
-                    code. I also enjoy petting every cat I see.
-                  </p>
-                  <div className="spacer-div mt-5 sm:mt-10"></div>
-                  <Titles />
-                  <div className="mt-5 xs:mt-12"></div>
-                  <div className="resume-btn-wrapper w-fit">
-                    <ResumeButton />
-                  </div>
+      <article className="aboutme-wrapper flex flex-row xs:gap-10">
+        <div className="Introduction__Wrapper grid lg:grid-cols-7 gap-5" id="introductionWrapper">
+          <div className="flex flex-col lg:col-span-4">
+            <Hi />
+            <div className="bio-description md:text-lg leading-10 text-textSmColor">
+              <div className="my-10 text-lg">
+                <p className="my-4">
+                  Welcome to my personal website!
+                </p>
+                <p className="mb-5 xs:mb-10">
+                  I enjoy building software that makes peoples' lives easier
+                  by writing elegant, performant, and maintainable frontend
+                  code. I also enjoy petting every cat I see.
+                </p>
+                <div className="spacer-div mt-5 sm:mt-10"></div>
+                <Titles />
+                <div className="mt-5 xs:mt-12"></div>
+                <div className="resume-btn-wrapper w-fit">
+                  <ResumeButton />
                 </div>
               </div>
             </div>
-
-            <div className="spacer-div sm:mt-10"></div>
           </div>
-
-        </div>
-        <SocialMedia/>
+          <div className="hidden md:flex mx-auto lg:col-span-3">
+            <ContactMeSection
+            fieldErrors={props.actionData && props.actionData.fieldErrors}
+            transition={props.transition}
+          />
+          </div>
+          </div>
+        <SocialMedia />
       </article>
-      
+      <div className="flex md:hidden justify-center">
+            <ContactMeSection
+            fieldErrors={props.actionData && props.actionData.fieldErrors}
+            transition={props.transition}
+          />
+          </div>
     </section>
   );
 };
