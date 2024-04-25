e109b6d1aef0fe2c12b8b9d5a4f05ca858fe7b6a
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-04-24 22:25:06 -0700

    Update resume button

--- app/components/ResumeButton/ResumeButton.tsx
+++ app/components/ResumeButton/ResumeButton.tsx
@@ -7,9 +7,9 @@ export const links: LinksFunction = () => {
 };
 const ResumeButton: React.FC = () => {
   return (
-    <a href="/resume.pdf" download={"AlissaNguyen_Resume.pdf"}>
+    <a href="/resume.pdf" download={"AlissaNguyen_Resume.pdf"} className="ResumeButton__Wrapper">
       <button
-        className="resume-button px-5 py-2 text-center uppercase relative after:absolute after:bottom-0 after:w-0 after:left-1/2 after:bg-transparent hover:cursor-pointer focus:cursor-pointer lg:text-base font-normal select-none rounded-xl"
+        className="slide text-lg"
         name="Download Resume"
         aria-label="Resume"
       >
