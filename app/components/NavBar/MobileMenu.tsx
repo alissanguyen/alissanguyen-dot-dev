7275bc98d09f5941d9770c81396dde86f795d1a2
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-06 17:54:20 -0700

    .

--- app/components/NavBar/MobileMenu.tsx
+++ app/components/NavBar/MobileMenu.tsx
@@ -1,9 +1,7 @@
-import { Menu, MenuButton } from "@reach/menu-button";
 import { useModalContext } from "~/providers/ModalProvider";
 import MobileMenuList from "./MobileMenuList";
 import * as React from "react";
 import { SupportedTheme } from "~/types";
-import "@reach/menu-button/styles.css"
 
 interface NavbarProps {
   theme: SupportedTheme;
@@ -32,54 +30,57 @@ const MobileMenu: React.FC<NavbarProps> = (props) => {
     props.theme,
     modalIsOpen
   );
-  return (
-    <Menu>
-      {({ isExpanded }) => {
-        React.useEffect(() => {
-          updateModalStatus(isExpanded);
-        }, [isExpanded]);
 
-        return (
-          <>
-            <MenuButton
-              id="menu--1"
-              aria-label={isExpanded ? "Close menu" : "Open menu"}
-              name={isExpanded ? "Close menu" : "Open menu"}
-              className={
-                "focus:outline-none inline-flex h-12 w-12 items-center justify-center rounded-full border-2 p-1 transition " +
-                className
-              }
-            >
-              <svg
-                className={`ham hamRotate ham8 ${modalIsOpen ? "active" : ""}`}
-                viewBox="0 0 100 100"
-                width="80"
-                id="ham-button"
-                fill="currentColor"
-              >
-                <path
-                  className="line top"
-                  d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
-                  fill="currentColor"
-                />
-                <path
-                  className="line middle"
-                  d="m 30,50 h 40"
-                  fill="currentColor"
-                />
-                <path
-                  className="line bottom"
-                  d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
-                  fill="currentColor"
-                />
-              </svg>
-            </MenuButton>
-            <MobileMenuList {...props} isExpanded={isExpanded} />
-          </>
-        );
-      }}
-    </Menu>
-  );
+  // Commenting out until we migrate off reach ui
+  return null
+  // return (
+  //   <Menu>
+  //     {({ isExpanded }) => {
+  //       React.useEffect(() => {
+  //         updateModalStatus(isExpanded);
+  //       }, [isExpanded]);
+
+  //       return (
+  //         <>
+  //           <MenuButton
+  //             id="menu--1"
+  //             aria-label={isExpanded ? "Close menu" : "Open menu"}
+  //             name={isExpanded ? "Close menu" : "Open menu"}
+  //             className={
+  //               "focus:outline-none inline-flex h-12 w-12 items-center justify-center rounded-full border-2 p-1 transition " +
+  //               className
+  //             }
+  //           >
+  //             <svg
+  //               className={`ham hamRotate ham8 ${modalIsOpen ? "active" : ""}`}
+  //               viewBox="0 0 100 100"
+  //               width="80"
+  //               id="ham-button"
+  //               fill="currentColor"
+  //             >
+  //               <path
+  //                 className="line top"
+  //                 d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
+  //                 fill="currentColor"
+  //               />
+  //               <path
+  //                 className="line middle"
+  //                 d="m 30,50 h 40"
+  //                 fill="currentColor"
+  //               />
+  //               <path
+  //                 className="line bottom"
+  //                 d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
+  //                 fill="currentColor"
+  //               />
+  //             </svg>
+  //           </MenuButton>
+  //           <MobileMenuList {...props} isExpanded={isExpanded} />
+  //         </>
+  //       );
+  //     }}
+  //   </Menu>
+  // );
 };
 
 export default MobileMenu;
--- app/components/NavBar/MobileMenuList.tsx
+++ app/components/NavBar/MobileMenuList.tsx
@@ -1,9 +1,7 @@
-import { MenuItems, MenuLink, MenuPopover } from "@reach/menu-button";
 import { AnimatePresence, motion, useReducedMotion } from "framer-motion";
 import { Link } from "@remix-run/react";
 import { topLevelLinksOnMobile } from "~/constants";
 import ThemeButton from "../ThemeButton/ThemeButton";
-import "@reach/menu-button/styles.css"
 
 interface NavbarProps {
 }
@@ -13,51 +11,53 @@ const MobileMenuList: React.FC<NavbarProps & { isExpanded: boolean }> = (
 ) => {
   const shouldReduceMotion = useReducedMotion();
 
-  return (
-    <AnimatePresence>
-      {props.isExpanded ? (
-        <MenuPopover
-          position={(mobileMenuExpandButton) => ({
-            top: `calc(${
-              Number(mobileMenuExpandButton?.top) +
-              Number(mobileMenuExpandButton?.height)
-            }px + 2.25rem)`, // 2.25 rem = py-9 from navbar
-            left: 0,
-            bottom: 0,
-            right: 0
-          })}
-          className="block z-50"
-        >
-          <motion.div
-            initial={{ y: -50, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: -50, opacity: 0 }}
-            transition={{
-              duration: shouldReduceMotion ? 0 : 0.15,
-              ease: "linear"
-            }}
-            className="MenuLists h-full overflow-y-scroll border-t border-mobileNav-border pb-12 outline-none"
-          >
-            <MenuItems className="MobileNav__MenuItemsWrapper flex border-none bg-transparent p-0 h-full flex-col">
-              {topLevelLinksOnMobile.map((link) => (
-                <MenuLink
-                  className="MobileNav__MenuItem border-b border-mobileNav-border px-[5vw] text-lg py-9"
-                  key={link.href}
-                  as={Link}
-                  to={link.href}
-                >
-                  {link.displayName}
-                </MenuLink>
-              ))}
-              <div className="noscript-hidden py-9 text-center flex justify-center">
-                <ThemeButton/>
-              </div>
-            </MenuItems>
-          </motion.div>
-        </MenuPopover>
-      ) : null}
-    </AnimatePresence>
-  );
+  // Commenting out until we migrate off reach UI
+  return null
+  // return (
+  //   <AnimatePresence>
+  //     {props.isExpanded ? (
+  //       <MenuPopover
+  //         position={(mobileMenuExpandButton) => ({
+  //           top: `calc(${
+  //             Number(mobileMenuExpandButton?.top) +
+  //             Number(mobileMenuExpandButton?.height)
+  //           }px + 2.25rem)`, // 2.25 rem = py-9 from navbar
+  //           left: 0,
+  //           bottom: 0,
+  //           right: 0
+  //         })}
+  //         className="block z-50"
+  //       >
+  //         <motion.div
+  //           initial={{ y: -50, opacity: 0 }}
+  //           animate={{ y: 0, opacity: 1 }}
+  //           exit={{ y: -50, opacity: 0 }}
+  //           transition={{
+  //             duration: shouldReduceMotion ? 0 : 0.15,
+  //             ease: "linear"
+  //           }}
+  //           className="MenuLists h-full overflow-y-scroll border-t border-mobileNav-border pb-12 outline-none"
+  //         >
+  //           <MenuItems className="MobileNav__MenuItemsWrapper flex border-none bg-transparent p-0 h-full flex-col">
+  //             {topLevelLinksOnMobile.map((link) => (
+  //               <MenuLink
+  //                 className="MobileNav__MenuItem border-b border-mobileNav-border px-[5vw] text-lg py-9"
+  //                 key={link.href}
+  //                 as={Link}
+  //                 to={link.href}
+  //               >
+  //                 {link.displayName}
+  //               </MenuLink>
+  //             ))}
+  //             <div className="noscript-hidden py-9 text-center flex justify-center">
+  //               <ThemeButton/>
+  //             </div>
+  //           </MenuItems>
+  //         </motion.div>
+  //       </MenuPopover>
+  //     ) : null}
+  //   </AnimatePresence>
+  // );
 };
 
 export default MobileMenuList;
