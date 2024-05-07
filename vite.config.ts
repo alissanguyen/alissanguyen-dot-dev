bdf176806eefb0331bb40e1a3580221e39f0b1d1
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-06 17:27:32 -0700

    install vercel thing.

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from "@vercel/remix/vite";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()]
+  plugins: [
+    remix({
+      presets: [vercelPreset()]
+    }),
+    tsconfigPaths()
+  ]
 });
