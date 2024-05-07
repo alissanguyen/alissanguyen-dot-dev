8ffeebd408214666142ab264203f95e9093df3e6
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2024-05-06 18:39:24 -0700

    Revert

--- remix.config.js
+++ remix.config.js
@@ -6,6 +6,6 @@ module.exports = {
   assetsBuildDirectory: "public/build",
   publicPath: "/build/",
   serverBuildDirectory: "api/_build",
-  devServerPort: 8002,
+  devServerPort: 8003,
   ignoredRouteFiles: [".*"],
 };
